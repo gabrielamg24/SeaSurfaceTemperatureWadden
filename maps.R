50126c4d749a0d5d6821ffcc33d1a4dc30db68e3
Iris <idbontekoe@hotmail.com>
2017-01-26 15:22:10 +0100

    Update maps.R

--- maps.R
+++ maps.R
@@ -1,21 +1,25 @@
+
+#Load necessary packages
 library(ggplot2)
-library(cowplot)
 library(ggmap)
-library(gganimate)
-library(grid)
 
-
-setwd("~/Downloads")
+#Load data
 data<-read.table("All_data.txt",sep=",",header=T)
-str(data)
+#str(data)
 
+#Load the satellite map
 lon<-mean(c(min(data$Longitude),max(data$Longitude)))
 lat<-mean(c(min(data$Latitude),max(data$Latitude)))
 map<-get_map(location=c(lon,lat),zoom=6,maptype="satellite")
 
-
+print("R is currently plotting your maps, this may take a while.")
+  
+#Find all unique year-month combinations
 year.month<-as.factor(unique(data$"Year.Month"))
+
+#This "for"-loopmakes a plot for every unique year-month combination
 for( i in seq_along(year.month)){
+#THe basic plot:
 p<-ggplot(data[data$"Year.Month"==year.month[i],],
 aes(Longitude,Latitude,color=Temperature))
 p<-ggmap(map,base_layer=p)+
@@ -31,5 +35,8 @@ axis.text =
 element_text(size=22),legend.title=element_text(size=18),legend.text=element_text(size=15))+
 theme(strip.text.x=element_blank())+
 theme(plot.background = element_rect(fill = "white",colour="white"))+ggtitle(year.month[i])
+#Save the plot into a .png file:
 ggsave(plot=p,filename=paste(year.month[i],".png",sep=""),width=8,height=8,dpi=100)
 }
+
+print("Your maps are plotted and will now be combined into a .gif file.")
